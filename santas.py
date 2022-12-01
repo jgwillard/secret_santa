4ba70b02ccb2d9062c1cc3b65eeaef1cbeb156da
John Willard <willard.john.gray@gmail.com>
2022-11-30 21:46:28 -0500

    update data format, other changes

--- santas.py
+++ santas.py
@@ -1,29 +1,54 @@
 #!/usr/bin/env python3
 
-import csv
 import os
-import random
+import json
 from string import Template
 import sys
+from typing import Dict, List
 
 from send import send_email
 
 user = os.environ.get('USER')
 password = os.environ.get('PASSWORD')
 
-csvfile = sys.argv[1]
+jsonfile = sys.argv[1]
 email_content = sys.argv[2]
 
 test_user = 'jgwil2'
 
-with open(csvfile) as f:
+def generateAdjacencyList(n, participants):
+    keys = participants.keys()
+    adjacency_list: Dict[str, List[str]] = {p: [] for p in participants}
+    for name, detail in participants.items():
+        for key in keys:
+            if key not in detail['excludes'] and key != name:
+                adjacency_list[name].append(key)
+    return adjacency_list
+
+
+def validPath(
+    n: int, adjacency_list: Dict[str, List[str]]
+) -> List[str]:
+    source = list(adjacency_list)[0]
+    target = source
+    visited = {}
+
+    # dfs
+    # TODO get valid path
+
+
+
+
+
+with open(jsonfile) as f:
     with open(email_content) as c:
         content = Template(c.read())
-        santas = list(csv.reader(f))
-        random.shuffle(santas)
-        l = len(santas)
-        for i, santa in enumerate(santas):
-            name, email = santa
-            k = (i + 1) % l
-            print('To:', santas[k][1])
-            print(content.substitute(name=name.capitalize()))
+        santas = json.load(f)
+        n = len(santas)
+        adjacency_list = generateAdjacencyList(n, santas)
+        print(validPath(n, adjacency_list))
+        #for i, santa in enumerate(santas):
+        #    name, email = santa
+        #    k = (i + 1) % l
+        #    print('To:', santas[k][1])
+        #    print(content.substitute(name=name.capitalize()))
