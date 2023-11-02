8a888fa72d4ce5c50e5c2144ecf944691cb77bfb
John Willard <willard.john.gray@gmail.com>
2023-11-01 20:04:44 -0400

    slight refactor to improve main script

--- santas.py
+++ santas.py
@@ -9,29 +9,29 @@ import sys
 from hamiltonian_cycle_finder import HamiltonianCycleFinder
 from send import send_email
 
-user = os.environ.get('USER')
-password = os.environ.get('PASSWORD')
-
-jsonfile = sys.argv[1]
-email_content = sys.argv[2]
-
 def randomize(santas):
     keys = list(santas.keys())
     random.shuffle(keys)
     return { key: santas[key] for key in keys }
 
 if __name__ == '__main__':
+    user = os.environ.get('USER')
+    password = os.environ.get('PASSWORD')
+
+    jsonfile = sys.argv[1]
+    email_content = sys.argv[2]
+
     with open(jsonfile) as f:
+        santas = json.load(f)
+        n = len(santas)
+        random_santas = randomize(santas)
+        cycle_finder = HamiltonianCycleFinder()
+        cycle_finder.validPath(n, random_santas)
+        santa_cycle = cycle_finder.solution
+        santa_cycle.pop()
+
         with open(email_content) as c:
             content = Template(c.read())
-            santas = json.load(f)
-            n = len(santas)
-            random_santas = randomize(santas)
-            cycle_finder = HamiltonianCycleFinder()
-            cycle_finder.validPath(n, random_santas)
-            santa_cycle = cycle_finder.solution
-            santa_cycle.pop()
-
             for i, santa in enumerate(santa_cycle):
                 name = santa
                 email = santas[name]['email']
